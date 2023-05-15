3e14636b21628f4c587ee3d559b5feb480f5b806
Wemerson Nino <wemersonnino@gmail.com>
2023-05-15 20:55:13 -0300

    Create logica calculos
    Create logica calculos

---
+++ app/src/main/java/com/example/trabalhodeandroidcalculadora/calculadoraFGTS.kt
@@ -0,0 +1,11 @@
+package com.example.trabalhodeandroidcalculadora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class calculadoraFGTS : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculadora_fgts)
+    }
+}
