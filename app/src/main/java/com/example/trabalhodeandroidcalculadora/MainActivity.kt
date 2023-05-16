ac4dc602c02c0eacc21886a60165eed4b5cd2328
Wemerson Nino <wemersonnino@gmail.com>
2023-05-15 23:52:40 -0300

    Commit logica

--- app/src/main/java/com/example/trabalhodeandroidcalculadora/MainActivity.kt
+++ app/src/main/java/com/example/trabalhodeandroidcalculadora/MainActivity.kt
@@ -6,6 +6,11 @@ import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    /*
+    * Prototipo Calculadora Financiera
+    * Alunos: Wemerson Pereira: 72100290
+    * Samuel Freitas: 72000830
+    * */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
